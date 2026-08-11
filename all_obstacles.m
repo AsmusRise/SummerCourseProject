76dd9ee6557c5f3cd091558ea0cf27e64d24c50d
AsmusRise <asris24@student.sdu.dk>
2026-08-11 13:56:35 +0200

    added floor

--- all_obstacles.m
+++ all_obstacles.m
@@ -14,6 +14,11 @@ p4 = [-144; -783; -30] * mm2m;
 edgeRadius = 0.02;
 pillarRadius = 0.03;
 
+%adding floor between the end lines:
+nPoints = 10;
+ptsAB = ( (1:nPoints)'/ (nPoints+1) ) .* (B - A)' + A'; % nPoints x 3
+ptsCD = ( (1:nPoints)'/ (nPoints+1) ) .* (D - C)' + C'; % nPoints x 3
+
 Obs = [
     A(1),  A(2),  A(3),  B(1),  B(2),  B(3),  edgeRadius
     B(1),  B(2),  B(3),  C(1),  C(2),  C(3),  edgeRadius
@@ -23,4 +28,16 @@ Obs = [
     p2(1), p2(2), p2(3), p2(1), p2(2), p2(3)+1, pillarRadius
     p3(1), p3(2), p3(3), p3(1), p3(2), p3(3)+1, pillarRadius
     p4(1), p4(2), p4(3), p4(1), p4(2), p4(3)+1, pillarRadius
+
+    %floor
+    ptsAB(1,1),ptsAB(1,2),ptsAB(1,3), ptsCD(1,1), ptsCD(1,2), ptsCD(1,3), edgeRadius
+    ptsAB(2,1),ptsAB(2,2),ptsAB(2,3), ptsCD(2,1), ptsCD(2,2), ptsCD(2,3), edgeRadius
+    ptsAB(3,1),ptsAB(3,2),ptsAB(3,3), ptsCD(3,1), ptsCD(3,2), ptsCD(3,3), edgeRadius
+    ptsAB(4,1),ptsAB(4,2),ptsAB(4,3), ptsCD(4,1), ptsCD(4,2), ptsCD(4,3), edgeRadius
+    ptsAB(5,1),ptsAB(5,2),ptsAB(5,3), ptsCD(5,1), ptsCD(5,2), ptsCD(5,3), edgeRadius
+    ptsAB(6,1),ptsAB(6,2),ptsAB(6,3), ptsCD(6,1), ptsCD(6,2), ptsCD(6,3), edgeRadius
+    ptsAB(7,1),ptsAB(7,2),ptsAB(7,3), ptsCD(7,1), ptsCD(7,2), ptsCD(7,3), edgeRadius
+    ptsAB(8,1),ptsAB(8,2),ptsAB(8,3), ptsCD(8,1), ptsCD(8,2), ptsCD(8,3), edgeRadius
+    ptsAB(9,1),ptsAB(9,2),ptsAB(9,3), ptsCD(9,1), ptsCD(9,2), ptsCD(9,3), edgeRadius
+    ptsAB(10,1),ptsAB(10,2),ptsAB(10,3), ptsCD(10,1), ptsCD(10,2), ptsCD(10,3), edgeRadius
 ];
