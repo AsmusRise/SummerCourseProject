07526e9073ff7d27303059c51eff10d33aa08a73
Thyme-alt <angelusfarkas@gmail.com>
2026-08-11 16:45:24 +0200

    useles main

--- all_obstacles.m
+++ all_obstacles.m
@@ -20,16 +20,20 @@ ptsAB = ( (1:nPoints)'/ (nPoints+1) ) .* (B - A)' + A'; % nPoints x 3
 ptsCD = ( (1:nPoints)'/ (nPoints+1) ) .* (D - C)' + C'; % nPoints x 3
 
 Obs = [
+    %{
     A(1),  A(2),  A(3),  B(1),  B(2),  B(3),  edgeRadius
     B(1),  B(2),  B(3),  C(1),  C(2),  C(3),  edgeRadius
     C(1),  C(2),  C(3),  D(1),  D(2),  D(3),  edgeRadius
     D(1),  D(2),  D(3),  A(1),  A(2),  A(3),  edgeRadius
+    %}
+
     p1(1), p1(2), p1(3), p1(1), p1(2), p1(3)+1, pillarRadius
     p2(1), p2(2), p2(3), p2(1), p2(2), p2(3)+1, pillarRadius
     p3(1), p3(2), p3(3), p3(1), p3(2), p3(3)+1, pillarRadius
     p4(1), p4(2), p4(3), p4(1), p4(2), p4(3)+1, pillarRadius
-
+];
     %floor
+    %{
     ptsAB(1,1),ptsAB(1,2),ptsAB(1,3), ptsCD(1,1), ptsCD(1,2), ptsCD(1,3), edgeRadius
     ptsAB(2,1),ptsAB(2,2),ptsAB(2,3), ptsCD(2,1), ptsCD(2,2), ptsCD(2,3), edgeRadius
     ptsAB(3,1),ptsAB(3,2),ptsAB(3,3), ptsCD(3,1), ptsCD(3,2), ptsCD(3,3), edgeRadius
@@ -41,3 +45,5 @@ Obs = [
     ptsAB(9,1),ptsAB(9,2),ptsAB(9,3), ptsCD(9,1), ptsCD(9,2), ptsCD(9,3), edgeRadius
     ptsAB(10,1),ptsAB(10,2),ptsAB(10,3), ptsCD(10,1), ptsCD(10,2), ptsCD(10,3), edgeRadius
 ];
+
+    %}
--- main.m
+++ main.m
@@ -1,38 +1,82 @@
 clear; clc; close all;
 ur5_kin = UR5Kinematics;
 
-C_ini = [0, -1.5, 0, -1.5, 0, 0];
 
-%coordinates of the goal
-Goal_cords_r=[0.100; -0.700 ; -0.300];
+%initial position
+C_ini = [-1.178, -1.57, -1.57, -1.57, 1.57, 0];
 
+%coordinates of the TCP in mm
+x=220;
+y=-670;
+z=18;
 %rotation of the TCP
-theta_x=0.4;
-theta_y=-0.2;
-theta_z=-5.5;
-    
+theta_x=0.7;
+theta_y=-3.018;
+theta_z=-0.013;
+
+%weights for route selection
+C_weights=[2 0.2 0.2 1 2 2];
+
+
+
+x_m= x/1000;
+y_m= y/1000;
+z_m= z/1000;
+Goal_cords_r=[x_m; y_m ; z_m];
 
 rx=[1 0 0 ; 0 cos(theta_x) -sin(theta_x); 0 sin(theta_x) cos(theta_x)];
 ry=[cos(theta_y) 0 sin(theta_y); 0 1 0 ; -sin(theta_y) 0 cos(theta_y)];
 rz=[cos(theta_z) -sin(theta_z) 0 ; sin(theta_z) cos(theta_z) 0; 0 0 1];
 
 %rotation matrix and transformation matrix
-R=rx*ry*rz;
+R=rz*ry*rx;
 T_be=[R Goal_cords_r; 0 0 0 1];
 
 %multiple join angles
 joint_angles=UR5Kinematics.inverse_kinematics(T_be, 0);
-
+disp(joint_angles)
 %obstacle collision checking for the goal joint angles and pick one for the
 %rrt to find a path to
 all_obstacles
-
+smallest_diff=1000;
 for i = 1:size(joint_angles, 1)
-    P = joint_angles(i, :);   % current row = one joint solution
-    collision_flag=UR5_collision_checking_Obs(ur5_kin, P, Obs);
+    C_goal = joint_angles(i, :);   % current row = one joint solution
+    collision_flag=UR5_collision_checking_Obs(ur5_kin, C_goal, Obs);
+    fprintf('Row %d: collision_flag = %d\n', i, collision_flag);
     if collision_flag==0
-        break
+        %if collision is zero check for angle distance
+         C_ini_deg=rad2deg(C_ini);
+         C_goal_deg=rad2deg(C_goal);
+         theta_diff=wrapToPi(C_ini-C_goal) .* C_weights;
+         disp(theta_diff)
+         total_diff=sum(abs(theta_diff));
+        
+         disp(total_diff)
+         if total_diff < smallest_diff
+         smallest_diff = total_diff;
+         smallest_Goal=C_goal;
+         end
     end
 end
-disp(P)
-[path, smooth_path] = MPExtendRRT(C_ini, P, Obs);
+
+if smallest_diff == 1000
+    fprintf('No collision-free joint angles exist\n');
+else
+    disp(smallest_Goal)
+    rad2deg(smallest_Goal)
+end
+    %{
+if collision_flag ~= 0
+    fprintf('no collision free joint angles exist');
+else
+    disp(C_goal);
+end
+    
+%[path, smooth_path] = MPExtendRRT(C_ini, P, Obs);
+
+C_goal= rad2deg(C_goal);
+joint_angles=rad2deg(joint_angles);
+disp(C_goal)
+disp(joint_angles)
+
+    %}
